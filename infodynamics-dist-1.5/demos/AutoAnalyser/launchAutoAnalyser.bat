3f93cfc949bb2883a891a7455b422b0586a02207
Roberto <r.murcio@ucl.ac.uk>
2020-02-05 00:41:54 +0000

    add new files

--- infodynamics-dist-1.5/demos/AutoAnalyser/launchAutoAnalyser.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-REM Run the AutoAnalyser launcher:
-java -classpath "..\..\infodynamics.jar" infodynamics.demos.autoanalysis.AutoAnalyserLauncher -use-current-dir
-
--- infodynamics-dist-1.5/demos/AutoAnalyser/launchAutoAnalyser.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Run the AutoAnalyser Launcher:
-java -classpath "../../infodynamics.jar" infodynamics.demos.autoanalysis.AutoAnalyserLauncher -use-current-dir
-
